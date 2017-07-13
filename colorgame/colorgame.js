c4f99542437716163955e6be916936870af62b47
Neil Opena <neil1114o@gmail.com>
2017-07-13 16:35:47 -0400

    Added new colors/play again button

--- colorgame/colorgame.js
+++ colorgame/colorgame.js
@@ -1,22 +1,19 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var goalColor = pickColor();
 var colorDisplay = document.querySelector("h1 span");
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetBtn = document.getElementById("reset");
 
 for(var i = 0; i <squares.length; i++){
 	squares[i].style.backgroundColor = colors[i];
 	squares[i].addEventListener("click",function(){
 		if(this.style.backgroundColor == goalColor){
 			messageDisplay.textContent = "Correct!";
+			h1.style.backgroundColor = goalColor;
+			resetBtn.textContent = "Play Again?";
 			changeColors(goalColor);
 		}else{
 			this.style.backgroundColor = "#232323";
@@ -24,6 +21,17 @@ for(var i = 0; i <squares.length; i++){
 		}
 	});
 }
+
+resetBtn.addEventListener("click",function(){
+	resetBtn.textContent = "New Colors";
+	colors = generateRandomColors(6);
+	goalColor = pickColor();
+	colorDisplay.textContent = goalColor;
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+});
 
 colorDisplay.textContent = goalColor;
 
@@ -35,4 +43,20 @@ function changeColors(color){
 
 function pickColor(){
 	return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function generateRandomColors(num){
+	var myArray = [];
+	for(var i = 0; i < num; i++){
+		myArray.push(randomColor());
+	}
+
+	return myArray;
+}
+
+function randomColor(){
+	var red = Math.floor(Math.random() * 256);
+	var green = Math.floor(Math.random() * 256);
+	var blue = Math.floor(Math.random() * 256);
+	return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
