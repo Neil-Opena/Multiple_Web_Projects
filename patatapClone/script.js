bf4b66b8cba682cecc309df6e7269acd2817ce8e
Neil Opena <neil1114o@gmail.com>
2017-07-15 00:25:23 -0400

    Added animations

--- patatapClone/script.js
+++ patatapClone/script.js
@@ -2,13 +2,3 @@
 		=colored paint that paints the screen
 		=raindrop animation
 		=randomized children circles
-	Random Circles at random locations
-	var height = window.innerHeight;
-	var width = window.innerWidth;
-	for(var i = 0; i < 100; i++){
-		var x = Math.floor(Math.random() * width);
-		var y = Math.floor(Math.random() * height);
-		var rad = Math.floor(Math.random() * 80);
-		var circle = new Path.Circle(new Point(x, y), rad);
-		circle.fillColor = "steelblue";
-	}
