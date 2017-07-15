72445cb41868237d4cf1f920d2d7ebdb6cf63108
Neil Opena <neil1114o@gmail.com>
2017-07-15 00:02:18 -0400

    generated random circles at random locations

--- patatapClone/script.js
+++ patatapClone/script.js
@@ -1,10 +1,14 @@
-// Create a Paper.js Path to draw a line into it:
-	var path = new Path();
-	// Give the stroke a color
-	path.strokeColor = 'black';
-	var start = new Point(100, 100);
-	// Move to start and draw a line from there
-	path.moveTo(start);
-	// Note the plus operator on Point objects.
-	// PaperScript does that for us, and much more!
-	path.lineTo(start + [ 100, -50 ]);
+	Ideas: 
+		=colored paint that paints the screen
+		=raindrop animation
+		=randomized children circles
+	Random Circles at random locations
+	var height = window.innerHeight;
+	var width = window.innerWidth;
+	for(var i = 0; i < 100; i++){
+		var x = Math.floor(Math.random() * width);
+		var y = Math.floor(Math.random() * height);
+		var rad = Math.floor(Math.random() * 80);
+		var circle = new Path.Circle(new Point(x, y), rad);
+		circle.fillColor = "steelblue";
+	}
