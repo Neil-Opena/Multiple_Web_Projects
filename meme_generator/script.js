eb998d751fd9e81073926aa5ba27110de0ba80ef
Neil Opena <neil1114o@gmail.com>
2017-07-30 10:09:42 -0400

    Added simple shapes

--- meme_generator/script.js
+++ meme_generator/script.js
@@ -1,4 +1,31 @@
 var c = document.querySelector('#c');
 var ctx = c.getContext("2d");
 
-var image = new Image();
+// ctx.fillRect(100,100,100,100);
+// ctx.strokeRect(50,50,50,50);
+
+// ctx.beginPath();
+// ctx.moveTo(10,10);
+// ctx.lineTo(50,50);
+// ctx.lineTo(50,10);
+// ctx.lineTo(10,10);
+// ctx.fill();
+ctx.strokeRect(50,50,100,100);
+
+ctx.beginPath();
+ctx.moveTo(75,75);
+ctx.lineTo(125,75);
+ctx.lineTo(125,125);
+ctx.fill();
+
+// var image = new Image();
+
+// image.onload = function(){
+//     console.log("Loaded image");
+//     ctx.drawImage(image, 0, 0, c.width, c.height);
+// }
+
+// // var savedImage = c.toDataURL();
+// // window.open(savedImage);
+
+// image.src = "images/fry.jpg";
